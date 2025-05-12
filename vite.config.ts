278f6c91195123443efcb29a248c1e4fabe049bf
reversefuture <2748551480@qq.com>
2025-05-12 10:21:53 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'), //确保绝对路径
     },
   },
   css: {
